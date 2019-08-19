a77dce425ba01b59b2b013739e1e0c87629e4b9d
stern-shawn <stern.shawn@gmail.com>
2019-08-18 21:23:12 -0700

    Improve comments, clean up dead lines

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-const numNodes = 16
+const numNodes = 16 // # of frequency bands
 const audioCtx = new AudioContext()
 const data = new Uint8Array(numNodes * 2)
 
+// Create an analyser node which pipes in the audio context and can be called to get FFT data at any instant in time.
+// FFT quality determined by the config values. Has no effect on downstream nodes in case we want to add effects later
 const analyserNode = new AnalyserNode(audioCtx, {
   fftSize: Math.max(numNodes, 32),
   maxDecibels: -20,
@@ -9,9 +11,9 @@ const analyserNode = new AnalyserNode(audioCtx, {
   smoothingTimeConstant: 0.8,
 })
 
-const volume = <HTMLElement>document.getElementById('volume')
 const elVisualizer = <HTMLElement>document.querySelector('.visualizer')
 
+// Create a series of nodes (one per frequency band) and keep their references so we can continually animate them
 const elNodes = Array.from({ length: numNodes }, (_, i) => {
   const node = document.createElement('div')
   node.className = 'node'
@@ -22,21 +24,22 @@ const elNodes = Array.from({ length: numNodes }, (_, i) => {
 })
 
 const updateVisualizer = () => {
+  // Keep it ~60fps 💸
   requestAnimationFrame(updateVisualizer)
 
-  // Put FFT frequency data into our array
+  // Get current FFT frequency data and stash it in our Uint8Array
   analyserNode.getByteFrequencyData(data)
 
   elNodes.forEach((node, i) => {
-    // Render the amplitude of current frequency and tint color
-    node.style.setProperty('--color', `${data[i]}`)
-    // Attempt a log-ish scale for sensitivity to higher registers as they drop off
+    // Render the amplitude of the current frequency, attempting a log-ish scale for sensitivity to higher registers as
+    // they drop off sooner. For style points lets also adjust the red tint of the bar based on amplitude.
     node.style.setProperty('--level', `${(data[i] / 255) * (1 + i / numNodes)}`)
+    node.style.setProperty('--color', `${data[i]}`)
   })
 }
 
 // Initiate the process of prompting the user for permission to access their audio, creating a 'source', and feeding
-// that source into the analyser node so we can get cool FFT data, etc
+// that source into the analyser node so we can get cool FFT data, etc. Finally, kick off the requestAnimationFrame loop
 const startStream = async () => {
   const audioStream = await navigator.mediaDevices.getUserMedia({
     audio: true,
@@ -48,7 +51,7 @@ const startStream = async () => {
   updateVisualizer()
 }
 
-// Let the user start things via a click event
+// Let the user consent to having their audio intercepted via a click event
 document.addEventListener('click', () => {
   // We need to programatically resume the audio stream if we do it this way on click
   // https://developers.google.com/web/updates/2017/09/autoplay-policy-changes#webaudio
